f5165492fa69ceac3275bd36fb3b7bfcc813ab10
Duddela Navya Sri <118823511+22BCE1794@users.noreply.github.com>
2025-10-23 19:09:27 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ load_dotenv()
 # -------------------------------
 # Paths
 # -------------------------------
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))  
 DATA_DIR = os.path.join(ROOT_DIR, "data") 
 
 
