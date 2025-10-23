4c43f2916e6add105f88eccee400bfbc884a83e8
Duddela Navya Sri <118823511+22BCE1794@users.noreply.github.com>
2025-10-23 19:01:51 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,12 +4,9 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import pandas as pd
 from dotenv import load_dotenv
-from recommender.item_cf import build_interaction_matrix, item_similarity_matrix, score_items_for_user
-from recommender.content_based import ContentScorer
-from llm import generate_explanation, LLM_MOCK
-
-
-
+from app.recommender.item_cf import build_interaction_matrix, item_similarity_matrix, score_items_for_user
+from app.recommender.content_based import ContentScorer
+from app.llm import generate_explanation, LLM_MOCK
 
 # Load environment variables
 load_dotenv()
